c21dbd7544eb0822af4f20da5df92dd17596f1c0
Flemming Berthelsen <flemming.berthelsen@gmail.com>
2024-11-22 15:57:54 +0000

    oprydning

--- supabase/migrations_backup_20241122_070657/20241122070230_add_type_to_detail_sections.sql
+++
@@ -1,7 +0,0 @@
--- Add type column to detail_sections
-ALTER TABLE "public"."detail_sections"
-ADD COLUMN "type" text NOT NULL DEFAULT 'text';
-
--- Update existing sections to have type 'text'
-UPDATE "public"."detail_sections"
-SET "type" = 'text';
